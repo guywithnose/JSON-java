9d86611318e77acdc9402729bb6dbaf13162b9c3
guywithnose <guywithnose@gmail.com>
2012-01-06 19:47:29 -0500

    Finished testing JSONWriter

--- src/org/json/tests/TestJSONWriter.java
+++ src/org/json/tests/TestJSONWriter.java
@@ -20,115 +20,171 @@ import junit.framework.TestCase;
 public class TestJSONWriter extends TestCase
 {
     JSONWriter jsonwriter;
-    
+
     class BadWriterThrowsOnNonBrace extends Writer
     {
 
-        /* (non-Javadoc)
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.io.Writer#write(char[], int, int)
          */
         @Override
         public void write(char[] cbuf, int off, int len) throws IOException
         {
-            if(cbuf[0] != '{')
+            if (cbuf[0] != '{')
                 throw new IOException("Test Message From Non-Brace");
         }
 
-        /* (non-Javadoc)
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.io.Writer#flush()
          */
         @Override
         public void flush() throws IOException
         {
-            
+
         }
 
-        /* (non-Javadoc)
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.io.Writer#close()
          */
         @Override
         public void close() throws IOException
         {
-            
+
         }
-        
+
     }
-    
-    class BadWriterThrowsOnBrace extends Writer
+
+    class BadWriterThrowsOnLeftBrace extends Writer
     {
 
-        /* (non-Javadoc)
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.io.Writer#write(char[], int, int)
          */
         @Override
         public void write(char[] cbuf, int off, int len) throws IOException
         {
-            if(cbuf[0] == '{')
-                throw new IOException("Test Message From Brace");
+            if (cbuf[0] == '{')
+                throw new IOException("Test Message From Left Brace");
         }
 
-        /* (non-Javadoc)
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.io.Writer#flush()
          */
         @Override
         public void flush() throws IOException
         {
-            
+
         }
 
-        /* (non-Javadoc)
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.io.Writer#close()
          */
         @Override
         public void close() throws IOException
         {
-            
+
         }
-        
+
+    }
+
+    class BadWriterThrowsOnRightBrace extends Writer
+    {
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.io.Writer#write(char[], int, int)
+         */
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException
+        {
+            if (cbuf[0] == '}')
+                throw new IOException("Test Message From Right Brace");
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.io.Writer#flush()
+         */
+        @Override
+        public void flush() throws IOException
+        {
+
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.io.Writer#close()
+         */
+        @Override
+        public void close() throws IOException
+        {
+
+        }
+
+    }
+
+    class BadExtensionThatCausesNestingError extends JSONStringer
+    {
+        public JSONWriter changeMode(char c)
+        {
+            mode = c;
+            return this;
+        }
     }
     
     public void testKey()
     {
         try
         {
-        jsonwriter = new JSONStringer();
-            String result = jsonwriter.object()
-                    .key("abc")
-                    .value("123")
-                    .key("abc2")
-                    .value(60)
-                    .key("abc3")
-                    .value(20.98)
-                    .key("abc4")
-                    .value(true)
-                    .endObject().toString();
-            assertEquals("{\"abc\":\"123\",\"abc2\":60,\"abc3\":20.98,\"abc4\":true}", result);
+            jsonwriter = new JSONStringer();
+            String result = jsonwriter.object().key("abc").value("123")
+                    .key("abc2").value(60).key("abc3").value(20.98).key("abc4")
+                    .value(true).endObject().toString();
+            assertEquals(
+                    "{\"abc\":\"123\",\"abc2\":60,\"abc3\":20.98,\"abc4\":true}",
+                    result);
         } catch (JSONException e)
         {
             fail(e.getMessage());
-        } 
+        }
     }
-    
+
     public void testValue()
     {
         try
         {
-        jsonwriter = new JSONStringer();
-            String result = jsonwriter.array().value("123").value(10).value(30.45).value(false).endArray().toString();
+            jsonwriter = new JSONStringer();
+            String result = jsonwriter.array().value("123").value(10)
+                    .value(30.45).value(false).endArray().toString();
             assertEquals("[\"123\",10,30.45,false]", result);
         } catch (JSONException e)
         {
             fail(e.getMessage());
-        } 
+        }
     }
-    
+
     public void testObject_StopsAtMaxDepth()
     {
         try
         {
             jsonwriter = new JSONStringer();
-            int i = 0;           
-            while(i < 201)
+            int i = 0;
+            while (i < 201)
             {
                 jsonwriter.object().key("123");
                 i++;
@@ -137,16 +193,16 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Nesting too deep.", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testArray_StopsAtMaxDepth()
     {
         try
         {
             jsonwriter = new JSONStringer();
-            int i = 0;           
-            while(i < 201)
+            int i = 0;
+            while (i < 201)
             {
                 jsonwriter.array();
                 i++;
@@ -155,9 +211,9 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Nesting too deep.", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testValue_OutOfSequence()
     {
         try
@@ -168,9 +224,9 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Value out of sequence.", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testObject_OutOfSequence()
     {
         try
@@ -181,35 +237,40 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Misplaced object.", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testObject_TwoObjectsWithinArray()
     {
         try
         {
             jsonwriter = new JSONStringer();
-            String result = jsonwriter.array().object().endObject().object().endObject().endArray().toString();
+            String result = jsonwriter.array().object().endObject().object()
+                    .endObject().endArray().toString();
             assertEquals("[{},{}]", result);
         } catch (JSONException e)
         {
             fail(e.getMessage());
         }
     }
-    
+
     public void testObject_TwoStringsAndAnIntWithinObject()
     {
         try
         {
             jsonwriter = new JSONStringer();
-            String result = jsonwriter.object().key("string1").value("abc").key("int").value(35).key("string2").value("123").endObject().toString();
-            assertEquals("{\"string1\":\"abc\",\"int\":35,\"string2\":\"123\"}", result);
+            String result = jsonwriter.object().key("string1").value("abc")
+                    .key("int").value(35).key("string2").value("123")
+                    .endObject().toString();
+            assertEquals(
+                    "{\"string1\":\"abc\",\"int\":35,\"string2\":\"123\"}",
+                    result);
         } catch (JSONException e)
         {
             fail(e.getMessage());
         }
     }
-    
+
     public void testKey_MisplacedKey()
     {
         try
@@ -220,9 +281,9 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Misplaced key.", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testKey_CatchesIoexception()
     {
         try
@@ -233,22 +294,22 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Test Message From Non-Brace", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testObject_CatchesIoexception()
     {
         try
         {
-            jsonwriter = new JSONWriter(new BadWriterThrowsOnBrace());
+            jsonwriter = new JSONWriter(new BadWriterThrowsOnLeftBrace());
             jsonwriter.object();
             fail("Should have thrown exception.");
         } catch (JSONException e)
         {
-            assertEquals("Test Message From Brace", e.getMessage());
-        } 
+            assertEquals("Test Message From Left Brace", e.getMessage());
+        }
     }
-    
+
     public void testKey_NullKey()
     {
         try
@@ -259,33 +320,129 @@ public class TestJSONWriter extends TestCase
         } catch (JSONException e)
         {
             assertEquals("Null key.", e.getMessage());
-        } 
+        }
     }
-    
+
     public void testArray_TwoArraysWithinObject()
     {
         try
         {
             jsonwriter = new JSONStringer();
-            String result = jsonwriter.object().key("123").array().endArray().key("1234").array().endArray().endObject().toString();
+            String result = jsonwriter.object().key("123").array().endArray()
+                    .key("1234").array().endArray().endObject().toString();
             assertEquals("{\"123\":[],\"1234\":[]}", result);
         } catch (JSONException e)
         {
             fail(e.getMessage());
         }
     }
-    
+
     public void testObject_TwoObjectsWithinObject()
     {
         try
         {
             jsonwriter = new JSONStringer();
-            String result = jsonwriter.object().key("123").object().endObject().key("1234").object().endObject().endObject().toString();
+            String result = jsonwriter.object().key("123").object().endObject()
+                    .key("1234").object().endObject().endObject().toString();
             assertEquals("{\"123\":{},\"1234\":{}}", result);
         } catch (JSONException e)
         {
             fail(e.getMessage());
         }
     }
-    
+
+    public void testArray_TwoArraysWithinArray()
+    {
+        try
+        {
+            jsonwriter = new JSONStringer();
+            String result = jsonwriter.array().array().endArray().array()
+                    .endArray().endArray().toString();
+            assertEquals("[[],[]]", result);
+        } catch (JSONException e)
+        {
+            fail(e.getMessage());
+        }
+    }
+
+    public void testArray_MisplacedArray()
+    {
+        try
+        {
+            jsonwriter = new JSONStringer();
+            jsonwriter.object().array();
+            fail("Should have thrown exception.");
+        } catch (JSONException e)
+        {
+            assertEquals("Misplaced array.", e.getMessage());
+        }
+    }
+
+    public void testEndArray_MisplacedEndArray()
+    {
+        try
+        {
+            jsonwriter = new JSONStringer();
+            jsonwriter.endArray();
+            fail("Should have thrown exception.");
+        } catch (JSONException e)
+        {
+            assertEquals("Misplaced endArray.", e.getMessage());
+        }
+    }
+
+    public void testEndObject_MisplacedEndObject()
+    {
+        try
+        {
+            jsonwriter = new JSONStringer();
+            jsonwriter.endObject();
+            fail("Should have thrown exception.");
+        } catch (JSONException e)
+        {
+            assertEquals("Misplaced endObject.", e.getMessage());
+        }
+    }
+
+    public void testEndObject_CatchesIoexception()
+    {
+        try
+        {
+            jsonwriter = new JSONWriter(new BadWriterThrowsOnRightBrace());
+            jsonwriter.object().endObject();
+            fail("Should have thrown exception.");
+        } catch (JSONException e)
+        {
+            assertEquals("Test Message From Right Brace", e.getMessage());
+        }
+    }
+
+    public void testPop_BadExtensionThatCausesNestingError1()
+    {
+        try
+        {
+            BadExtensionThatCausesNestingError betcnw = new BadExtensionThatCausesNestingError();
+            betcnw.object().endObject();
+            betcnw.changeMode('k').endObject();
+            fail("Should have thrown exception.");
+        } catch (JSONException e)
+        {
+            assertEquals("Nesting error.", e.getMessage());
+        }
+    }
+
+    public void testPop_BadExtensionThatCausesNestingError2()
+    {
+        try
+        {
+            BadExtensionThatCausesNestingError betcnw = new BadExtensionThatCausesNestingError();
+            betcnw.array();
+            betcnw.changeMode('k').endObject();
+            fail("Should have thrown exception.");
+        } catch (JSONException e)
+        {
+            assertEquals("Nesting error.", e.getMessage());
+        }
+    }
+
 }
